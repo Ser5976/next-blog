a84f69b608685692fb40f675d0d7f5a9dcd8c75d
Seriy <tsbaranova@bk.ru>
2025-11-02 13:51:50 +0300

    jest.setup.ts typeof global.TextEncoder

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,5 +3,5 @@ import '@testing-library/jest-dom';
 // Минимальная настройка для тестов без node-fetch
 import { TextDecoder, TextEncoder } from 'util';
 
-global.TextEncoder = TextEncoder;
+global.TextEncoder = TextEncoder as typeof global.TextEncoder;
 global.TextDecoder = TextDecoder as typeof global.TextDecoder;
